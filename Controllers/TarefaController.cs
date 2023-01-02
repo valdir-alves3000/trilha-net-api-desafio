bbfddd1ec4f3baf77310d7826aefb5d0ed7f6ee2
Valdir Alves <valdiralves@gmail.com>
2023-01-02 19:19:31 -0300

    Challenge completed successfully

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -4,93 +4,114 @@ using TrilhaApiDesafio.Models;
 
 namespace TrilhaApiDesafio.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class TarefaController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class TarefaController : ControllerBase
+  {
+    private readonly OrganizadorContext _context;
+
+    public TarefaController(OrganizadorContext context)
     {
-        private readonly OrganizadorContext _context;
-
-        public TarefaController(OrganizadorContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult ObterPorId(int id)
-        {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
-        }
-
-        [HttpGet("ObterTodos")]
-        public IActionResult ObterTodos()
-        {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
-        }
-
-        [HttpGet("ObterPorTitulo")]
-        public IActionResult ObterPorTitulo(string titulo)
-        {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
-        }
-
-        [HttpGet("ObterPorData")]
-        public IActionResult ObterPorData(DateTime data)
-        {
-            var tarefa = _context.Tarefas.Where(x => x.Data.Date == data.Date);
-            return Ok(tarefa);
-        }
-
-        [HttpGet("ObterPorStatus")]
-        public IActionResult ObterPorStatus(EnumStatusTarefa status)
-        {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            var tarefa = _context.Tarefas.Where(x => x.Status == status);
-            return Ok(tarefa);
-        }
-
-        [HttpPost]
-        public IActionResult Criar(Tarefa tarefa)
-        {
-            if (tarefa.Data == DateTime.MinValue)
-                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
-
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
-            return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Atualizar(int id, Tarefa tarefa)
-        {
-            var tarefaBanco = _context.Tarefas.Find(id);
-
-            if (tarefaBanco == null)
-                return NotFound();
-
-            if (tarefa.Data == DateTime.MinValue)
-                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
-
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            return Ok();
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Deletar(int id)
-        {
-            var tarefaBanco = _context.Tarefas.Find(id);
-
-            if (tarefaBanco == null)
-                return NotFound();
-
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
-            return NoContent();
-        }
+      _context = context;
     }
+
+    [HttpGet("{id}")]
+    public IActionResult ObterPorId(int id)
+    {
+      var tarefa = _context.Tarefas.Find(id);
+
+      if (tarefa == null)
+        return NotFound();
+
+      return Ok(tarefa);
+    }
+
+    [HttpGet("ObterTodos")]
+    public IActionResult ObterTodos()
+    {
+      var tarefas = _context.Tarefas.ToList();
+
+      return Ok(tarefas);
+    }
+
+    [HttpGet("ObterPorTitulo")]
+    public IActionResult ObterPorTitulo(string titulo)
+    {
+      var tarefa = _context.Tarefas.Where(x => x.Titulo == titulo);
+
+      if (tarefa == null)
+        return NotFound();
+
+      return Ok(tarefa);
+    }
+
+    [HttpGet("ObterPorData")]
+    public IActionResult ObterPorData(DateTime data)
+    {
+      var tarefas = _context.Tarefas.Where(x => x.Data.Date == data.Date);
+
+      if (tarefas == null)
+        return NotFound();
+
+      return Ok(tarefas);
+    }
+
+    [HttpGet("ObterPorStatus")]
+    public IActionResult ObterPorStatus(EnumStatusTarefa status)
+    {
+      var tarefas = _context.Tarefas.Where(x => x.Status == status);
+
+      if (tarefas == null)
+        return NotFound();
+
+      return Ok(tarefas);
+    }
+
+    [HttpPost]
+    public IActionResult Criar(Tarefa tarefa)
+    {
+      if (tarefa.Data == DateTime.MinValue)
+        return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+
+      _context.Add(tarefa);
+      _context.SaveChanges();
+
+      return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Atualizar(int id, Tarefa tarefa)
+    {
+      var tarefaBanco = _context.Tarefas.Find(id);
+
+      if (tarefaBanco == null)
+        return NotFound();
+
+      if (tarefa.Data == DateTime.MinValue)
+        return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+
+      tarefaBanco.Data = tarefa.Data;
+      tarefaBanco.Descricao = tarefa.Descricao;
+      tarefaBanco.Titulo = tarefa.Titulo;
+
+      _context.Tarefas.Update(tarefaBanco);
+      _context.SaveChanges();
+
+      return Ok(tarefa);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Deletar(int id)
+    {
+      var tarefaBanco = _context.Tarefas.Find(id);
+
+      if (tarefaBanco == null)
+        return NotFound();
+
+      _context.Tarefas.Remove(tarefaBanco);
+      _context.SaveChanges();
+
+      return NoContent();
+    }
+  }
 }
